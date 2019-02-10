21cea9dc9546e94592e4f41a89d92a5a6cdddf96
Vanildo Prates <vprates@gmail.com>
2019-02-10 15:26:11 -0200

    Backend app

---
+++ src/controllers/TweetController.js
@@ -0,0 +1,14 @@
+const Tweet = require('../models/Tweet');
+
+module.exports = {
+  async index(req, res) {
+    const tweets = await Tweet.find({}).sort('-created_at');
+    return res.json(tweets);
+  },
+
+  async store(req, res){
+    const tweet = await Tweet.create(req.body);
+    req.io.emit('tweet', tweet);
+    return res.json(tweet);
+  }
+}
